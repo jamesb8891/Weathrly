757966cf6395eac3935cfa8a989771f41b1b2475
James Burbage <jeburbage@gmail.com>
2018-09-10 16:47:18 -0600

    Fix last page next button, button hidden after 10 days or 10 hours

--- src/DailyWeather.js
+++ src/DailyWeather.js
@@ -6,35 +6,38 @@ import WeatherIcons from './WeatherIcons';
 const dailyWeather = (props) => {
   return (
     <div className='main'>
-    <header>
-      <article className='left-header'>
-        <Controller 
-        fetchWeather={props.fetchWeather} 
-        location={props.location}
-        fetchZipCode={props.fetchZipCode}
-        />
-        <div className='date-controller'>
-          <p className='date'>{props.dailyWeather.date.weekday_short},
+      <header>
+        <article className='left-header'>
+          <Controller
+            fetchWeather={props.fetchWeather}
+            location={props.location}
+            fetchZipCode={props.fetchZipCode}
+          />
+          <div className='date-controller'>
+            <p className='date'>{props.dailyWeather.date.weekday_short},
             <span> {props.dailyWeather.date.monthname_short}</span>
-            <span> {props.dailyWeather.date.day}</span>
-            <span> {props.dailyWeather.date.year}</span>
-          </p>
-          <button onClick={props.controlPeriod} className='prev-day'>-</button>
-          <button onClick={props.controlPeriod} className='next-day'>+</button>
-        </div>
-        <p className='time'>{props.dailyWeather.date.pretty.slice(0, -25)}</p>
-      </article>
-      <article className='right-header'>
-        <p className='big-temp'>{props.dailyWeather.high.fahrenheit}˚</p>
-      </article>
-    </header>
-    <section className='icon'>
-      <img className='big-icon'src={WeatherIcons[props.dailyWeather.icon]} alt=''/> 
-    </section>
-    <section className='conditions'>
-      <p className='string-conditions'>{props.dailyWeather.conditions}</p>
-    </section>
-    </div>
+              <span> {props.dailyWeather.date.day}</span>
+              <span> {props.dailyWeather.date.year}</span>
+            </p>
+            <button onClick={props.controlPeriod} className='prev-day'>-</button>
+            {
+              props.dailyPeriod < 9 &&
+              <button onClick={props.controlPeriod} className='next-day'>+</button>
+            }
+          </div>
+          <p className='time'>{props.dailyWeather.date.pretty.slice(0, -25)}</p>
+        </article>
+        <article className='right-header'>
+          <p className='big-temp'>{props.dailyWeather.high.fahrenheit}˚</p>
+        </article>
+      </header>
+      <section className='icon'>
+        <img className='big-icon' src={WeatherIcons[props.dailyWeather.icon]} alt='' />
+      </section>
+      <section className='conditions'>
+        <p className='string-conditions'>{props.dailyWeather.conditions}</p>
+      </section>
+    </div >
   )
 }
 
