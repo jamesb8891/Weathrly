913537455ac327a1ff0d2bde7d78d63e96ed9392
James Burbage <jeburbage@gmail.com>
2018-09-11 15:20:54 -0600

    Testing start

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow, mount } from 'enzyme';
-import App from './App';
+import React from "react";
+import ReactDOM from 'react-dom'
+import { shallow, mount } from "enzyme";
+import App from "./App";
 
-describe('App', () => {
-  it('should shallow', () => {
-    let component = shallow(<App />);
+describe("App", () => {
+  let wrapper;
 
-    console.log( component.debug() );
+  beforeEach(() => {
+    wrapper = shallow(<App />);
   });
 
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  it("should exist", () => {
+    expect(wrapper).toBeDefined();
   });
-});
+
+  it("should render the HourlyWeather and DailyWeather component", () => {
+    expect(wrapper.find("HourlyWeather")).toBeDefined();
+    expect(wrapper.find("DailyWeather")).toBeDefined();
+  });
+
+  it("initially should have a state of location set to an empty string", () => {
+    expect(wrapper.state()).toEqual({
+      location: '', currentWeather: null, hourlyWeather: null, hourlyPeriod: 0, dailyWeather: null, dailyPeriod: 0,
+    });
+  });
+
+  it("should retrieve data from local storage", () => {
+    const location = [
+      { title: 'location', id: 1 }
+    ];
+
+    // localStorage.setItem(location, location);
+
+    wrapper = mount(<App />);
+    expect(wrapper.state()).toEqual({ "currentWeather": null, "dailyPeriod": 0, "dailyWeather": null, "hourlyPeriod": 0, "hourlyWeather": null, "location": "" });
+  });
+})
